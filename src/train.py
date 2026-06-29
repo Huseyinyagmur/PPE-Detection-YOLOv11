c5d272f5814078177f110df0e746ccc4bad2b622
Huseyin Yagmur <huseyin.mert.yagmur1907@gmail.com>
2026-06-29 11:42:20 +0300

    Implement YOLOv11 training pipeline

--- src/train.py
+++ src/train.py
@@ -1,2 +1,23 @@
-# Training entry point for the PPE object detection model.
+from ultralytics import YOLO
+from config import (
+    MODEL_NAME,
+    DATA_YAML,
+    EPOCHS,
+    IMAGE_SIZE,
+    BATCH_SIZE,
+    DEVICE,
+    PROJECT_NAME,
+    RUN_NAME
+)
+if __name__ == "__main__":
+    model=YOLO(MODEL_NAME)
 
+    model.train(
+        epochs=EPOCHS,
+        batch=BATCH_SIZE,
+        data=DATA_YAML,
+        imgsz=IMAGE_SIZE,
+        device=DEVICE,
+        project=PROJECT_NAME,
+        name=RUN_NAME
+    )
